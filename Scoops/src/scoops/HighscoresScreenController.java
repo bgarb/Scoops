aff8cffab9dd9a683aec5412fe97ae90a9b390b3
Jennifer Jiao <jjennijiao@gmail.com>
2015-05-05 14:05:25 -0500

    GetY method

---
+++ Scoops/src/scoops/HighscoresScreenController.java
@@ -0,0 +1,52 @@
+package scoops;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author csstudent
+ */
+public class HighscoresScreenController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void startScreenButton(ActionEvent event) {
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("OfficialStartScreen.fxml"));
+            Scene scene = new Scene(root);
+            Scoops.mainWindow.setScene(scene);
+            Scoops.mainWindow.show();
+        } catch (IOException ex) {
+            Logger.getLogger(HighscoresScreenController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
