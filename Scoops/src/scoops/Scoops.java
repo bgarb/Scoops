d7259daa77a85fc80b73a9f6ac01fb3db167211a
Jennifer Jiao <jjennijiao@gmail.com>
2015-05-07 13:44:09 -0500

    update on scoopanimation - ewhitehouse jjiao

--- Scoops/src/scoops/Scoops.java
+++ Scoops/src/scoops/Scoops.java
@@ -1,15 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package scoops;
 
+import javafx.animation.Animation;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 
 /**
@@ -17,15 +17,35 @@ import javafx.stage.Stage;
  * @author csstudent
  */
 public class Scoops extends Application {
+    
     static Stage mainWindow;
+    private static final Image IceCream = new Image("http://foodservice.bluebunny.com/content/cms/products/204x154/french_vanilla_3gal.s3v1.png");
+    private static final int COLUMNS  =   4;
+    private static final int COUNT    =  10;
+    private static final int OFFSET_X =  0;
+    private static final int OFFSET_Y =  5;
+    private static final int WIDTH    = 204;
+    private static final int HEIGHT   = 154;
     
     @Override
     public void start(Stage stage) throws Exception {
         mainWindow = stage;
         Parent root = FXMLLoader.load(getClass().getResource("OfficalStartScreen.fxml"));
         
+        final ImageView imageView = new ImageView(IceCream);
+        imageView.setViewport(new Rectangle2D(OFFSET_X, OFFSET_Y, WIDTH, HEIGHT));
+
+        final Animation animation = new IceCreamAnimation(
+                imageView,
+                Duration.millis(1000),
+                COUNT, COLUMNS,
+                OFFSET_X, OFFSET_Y,
+                WIDTH, HEIGHT
+        );
+        animation.setCycleCount(Animation.INDEFINITE);
+        animation.play();
+    
         Scene scene = new Scene(root);
-        
         stage.setScene(scene);
         stage.show();
     }
@@ -37,4 +57,16 @@ public class Scoops extends Application {
         launch(args);
     }
     
+
+   /* 
+    private ImageView iceCream1;
+    public void collisionDectection(){
+        int retval = Double.compare(iceCream1.yProperty(), 101);
+        if(retval < 0){
+           iceCream1.setValue(visible) = false; 
+        }
+    }                  
+
+
 }
+
