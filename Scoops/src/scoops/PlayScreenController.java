d81906e7acec0763deecfec68c7c9fbdd065c0d7
lportugal <lportugal@10.150.105.74>
2015-06-03 13:43:01 -0500

    Added proper code to make cone move functionally

--- Scoops/src/scoops/PlayScreenController.java
+++ Scoops/src/scoops/PlayScreenController.java
@@ -5,6 +5,12 @@
  */
 package scoops;
 
+import javafx.animation.Animation;
+
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -15,19 +21,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
 import javafx.scene.image.ImageView;
 import static javafx.scene.input.KeyCode.*;
 import javafx.scene.input.KeyEvent;
-import javafx.animation.AnimationTimer;
-import javafx.animation.Animation;
-
-import javafx.geometry.Rectangle2D;
-import javafx.scene.image.Image;
-import javafx.scene.layout.AnchorPane;
-import javafx.util.Duration;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 
 
 
@@ -43,102 +40,66 @@ import javafx.event.EventHandler;
  */
 
 public class PlayScreenController implements Initializable {
-  
-       
     Image IceCream = new Image("http://40.media.tumblr.com/89c8768c2980e4301383e9dfebc6a3e4/tumblr_nopqe9zTje1uvakkzo1_100.png");
-   // public static final int COLUMNS  =   4;
-    //public static final int COUNT    =  10;
-   public static final int OFFSET_X =  0;
+    public static final int OFFSET_X =  0;
     public static  int OFFSET_Y =  0;
     public static final int WIDTH    = 204;
     public static final int HEIGHT   = 174;
     
+    private static final int KEYBOARD_MOVEMENT = 10;
+    
     @Override
-    public void initialize(URL url, ResourceBundle rb) {    
-        
-        
-        ImageView imageView = new ImageView(IceCream);
-        imageView.setViewport(new Rectangle2D(OFFSET_X, OFFSET_Y, WIDTH, HEIGHT));
-        mainPane.getChildren().add(imageView);
-       /** int scaledImageSizeW;
-        
-        scaledImageSizeW = (int) IceCream.getWidth()/2;
-        
-       int scaledImageSizeH;
-        
-        scaledImageSizeH = (int) IceCream.getHeight()/2;
-         imageView.setScaleX(1/2);
-         imageView.setScaleY(1/2);**/
-         
-         
-         
-       final Animation animation = new IceCreamAnimation(
-                imageView,
-                Duration.millis(1500),
-                mainPane.getHeight(),
-                 OFFSET_Y,
-                WIDTH, HEIGHT
-        );
-        animation.setCycleCount(1);
-        animation.setOnFinished(new EventHandler<ActionEvent>() {
-            
-       @Override
-       public void handle(ActionEvent event){
-           
-       imageView.setX(Math.random()*400);
-       animation.play();
-       
-       }
-               
-    
-    });
-        animation.play();
-    
-        
-      
+    public void initialize(URL url, ResourceBundle rb) {  
     }
-
-    @FXML
-    private javafx.scene.control.Label label;
-    private int conePosition;
-    private long keyPressed;
-
     @FXML
     private ImageView cone;
     
+    private Scene scene;
+    
     @FXML
     private AnchorPane mainPane;
-
+        
+    /*ImageView imageView = new ImageView(IceCream);
+    
+    imageView.setViewport(new Rectangle2D(OFFSET_X, OFFSET_Y, WIDTH, HEIGHT));
+    mainPane.getChildren().add(imageView);
+         
+    final Animation animation = new IceCreamAnimation(
+        imageView,
+        Duration.millis(1500),
+        mainPane.getHeight(),
+        OFFSET_Y,
+        WIDTH, HEIGHT
+    );
+    animation.setCycleCount(1);
+    animation.setOnFinished(new EventHandler<ActionEvent>() {
+        
+        @Override
+        public void handle(ActionEvent event){    
+            imageView.setX(Math.random()*400);
+            animation.play();
+        }
+    });
+    animation.play();*/
+    
+    
     @FXML
-    private void handleKeyPressed(KeyEvent event) {
-        KeyCode key = event.getCode();
-        conePosition = 0;
-        keyPressed = 0;
-        new AnimationTimer() {
-            @Override
-            public void handle(long now) {
-                if (keyPressed == 0) {
-                    keyPressed = now;
-                } else if (now - keyPressed > 200) {
-                    if (key == RIGHT) {
-                        conePosition++;
-                        keyPressed = now;
-                    } else if (key == LEFT) {
-                        conePosition--;
-                        keyPressed = now;
-                    }
-
-                    cone.setX(conePosition);
-                }
-
-            }
-        }.start();
+     void setScene(Scene scene) {
+        this.scene = scene;
     }
     
+    @FXML
+    private void moveConeOnKeyPress(KeyEvent event) {
+        //playScreen = getPlayScreen();
+        scene.setOnKeyPressed((KeyEvent keyEvent) -> {
+            switch (keyEvent.getCode()) {
+                case RIGHT: cone.setX(cone.getX() + KEYBOARD_MOVEMENT); break;
+                case LEFT:  cone.setX(cone.getX() - KEYBOARD_MOVEMENT); break;
+            }
+        });
+    }
     
-   
-        @FXML
-    
+    @FXML
     private void quitButton(ActionEvent event) {
         Parent root;
         try {
@@ -149,9 +110,5 @@ public class PlayScreenController implements Initializable {
         } catch (IOException ex) {
             Logger.getLogger(StartScreenController.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }
-
-    
-
-   
+    }  
 }
